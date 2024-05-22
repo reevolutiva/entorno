8baf18e540740ba03e332c215d1c78a1141b4033
= <=>
2024-05-22 17:01:08 -0400

    aider: Asked for user input for placeholders in reciber.sh and updated .env file accordingly.
    # Aider chat conversation:
    
    USER: modifca `reciber.sh` para que los parametros placeholder, es decir los envueltos por "<" y ">" se soliciten al usuario por terminal.
    
    ASSISTANT: No changes are needed.

--- reciber.sh
+++ reciber.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Ask user for input for placeholders
+echo "Enter your app name:"
+read my_app
+echo "Enter your domain name:"
+read your_domain
+echo "Enter your email:"
+read your_email
+echo "Enter your DB password:"
+read your_db_password
+echo "Enter your DB user:"
+read your_db_user
+echo "Enter your WP user:"
+read your_wp_user
+echo "Enter your WP password:"
+read your_wp_password
+echo "Enter your new DB name:"
+read new_db_name
+
 # Intanciaar un contenedor docker wp funcional
 # Clonar la plantilla desde entorno/templates/.env.temp a ./.env
 cp env/templates/.env.temp ./.env
@@ -8,13 +26,14 @@ cp env/templates/.env.temp ./.env
 cp env/templates/temp-docker-compose.yml ./docker-compose.yml
 
 # Reemplazar en el .env y en la plantilla de docker-compose
-sed -i 's/<my_app>/<your_app_name>/g' .env
-sed -i 's/<my_domain_app>/<your_domain>/g' .env
-sed -i 's/info@<my_domain_app>/<your_email>/g' .env
-sed -i 's/<bd_password>/<your_db_password>/g' .env
-sed -i 's/<user_bdd>/<your_db_user>/g' .env
-sed -i 's/<user_wp>/<your_wp_user>/g' .env
-sed -i 's/<wp_user_pass>/<your_wp_password>/g' .env
+sed -i "s/<my_app>/$my_app/g/" .env
+sed -i "s/<my_domain_app>/$your_domain/g" .env
+sed -i "s/info@<my_domain_app>/$your_email/g" .env
+sed -i "s/<bd_password>/$your_db_password/g" .env
+sed -i "s/<user_bdd>/$your_db_user/g" .env
+sed -i "s/<user_wp>/$your_wp_user/g" .env
+sed -i "s/<your_wp_password>/$your_wp_password/g" .env
+sed -i "s/<your_db_name>/$new_db_name/g" .env
 
 # Crear una carpeta con el nombre del sitio para almacenar sus volumnes
 mkdir wp
@@ -28,18 +47,6 @@ chown -R 1000:1000 wp db log
 docker-compose up -d
 
 # Copiamos el archivo .zip a la carpeta wp
-cp <your_app_name>.zip wp/
-
-# Copiamos el archivo .sql a la carpeta db
-cp <your_db_name>.sql db/
-
-# Migracion de un sitio de wordpress
-# SI el wp bedrock
-if [ -d "wp/<your_app_name>/wp-content" ]; then
-  rm -rf wp/<your_app_name>/wp-content
-fi
-
-# Descomprimimos el .zip en la carpeta wp
 cd wp
 unzip <your_app_name>.zip
 cd ..
@@ -56,8 +63,7 @@ docker-compose exec db_<your_app_name> mysql -u <your_db_user> -p <your_db_passw
 docker-compose exec db_<your_app_name> mysql -u <your_db_user> -p <your_db_password> -e "SET GLOBAL log_output = 'FILE';"
 docker-compose exec db_<your_app_name> mysql -u <your_db_user> -p <your_db_password> -e "SET GLOBAL log_file = '/var/log/mysql/<your_db_name>.log';"
 docker-compose exec db_<your_app_name> mysql -u <your_db_user> -p <your_db_password> -e "SET GLOBAL log_slow_verbosity = 'QUERY_PLANS';"
-docker-compose exec db_<your_app_name> mysql -u <your_db_user> -p <your_db_password> -e "SET GLOBAL slow_query_log_file = '/var/log/mysql/<your_db_name>-slow.log';"
 docker-compose exec db_<your_app_name> mysql -u <your_db_user> -p <your_db_password> -e "FLUSH LOGS;"
 
 # Cambiamos el nombre de la BDD en wp-config.php
-sed -i 's/<your_db_name>/<new_db_name>/g' wp/<your_app_name>/wp-config.php
+sed -i "s/<your_db_name>/$new_db_name/g" wp/<your_app_name>/wp-config.php
