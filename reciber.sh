937979f4703b1712af87777188ee0e95d7ef42ad
= <=>
2024-05-22 16:31:12 -0400

    aider: Instanciaar un contenedor docker wp funcional
    # Aider chat conversation:
    
    USER: Por favor completa interpereta el psedudo codigo que escribi en este archivo y escribelo en codigo de bash
    
    ASSISTANT: No changes are needed.

--- reciber.sh
+++ reciber.sh
@@ -1,39 +1,44 @@
-# Intanciaar un contenedor docker wp funcional
-## Clonar la plantilla del .env
-## Clonar la plantilla del docker-compose
-## Reemplazar en el .env y en la plantillade docker-compose
-### Reemplace <my_app> por el nombre de su app.
-### Reemplace <my_domain_app> por el dominio de su app
-### Reemplace info@<my_domain_app> un correo electrónico real.
-### Reemplace <bd_password> una contraseña real.
-### Reemplace <user_bdd> un nombre de usuario para BDD.
-### Reemplace <user_wp>un nombre de usuario para Wordpress.
-### Reemplace <wp_user_pass> una contraseña real.
-## Crear una carpeta con el nombre del sitio para almacenar sus volumnes
-### Crear carpeta wp
-### Creare carpeta db
-### Crear carpeta log
+#!/bin/bash
 
-## Cambiamos el owner de la carpeta y todas su subcarpetas por el usuario 1000
-## Levantamos el docker-compose.yml
+# Instanciaar un contenedor docker wp funcional
 
-# Migracion de un sitio de wordpress
-## SI el wp bedrock
-## Eliminamos la carpeta web
-## Entonces
-## Eliminamos la carpeta wp-content
+# Clonar la plantilla del .env
+cp .env.template .env
 
-## Copiamos el archivo .zip a la carpeta wp
-## Copiamos el archivo .sql a la carpeta db
+# Clonar la plantilla del docker-compose
+cp docker-compose.template.yml docker-compose.yml
 
-## Descomprimimos el .zip en la carpeta wp
-## Cambiamos los permisos de todos los ficheros dentro de la carpeta wp
+# Reemplazar en el .env y en la plantilla de docker-compose
+sed -i "s/<my_app>/${1}/g" .env
+sed -i "s/<my_domain_app>/${2}/g" .env
+sed -i "s/info@<my_domain_app>/${3}/g" .env
+sed -i "s/<bd_password>/${4}/g" .env
+sed -i "s/<user_bdd>/${5}/g" .env
+sed -i "s/<user_wp>/${6}/g" .env
+sed -i "s/<wp_user_pass>/${7}/g" .env
 
-## Entramos a la terminal del contenedor mysql.
-## Entramos a mysql.
-## Creamos una BDD vacia con el mismo nombre que la original.
-## Entramos en la BDD.
-## Importamos el .sql con SOURCE
-## Le damos los permisos al usuario de la BDD
-## Cambiamos el nombre de la BDD en wp-config.php
+sed -i "s/<my_app>/${1}/g" docker-compose.yml
+sed -i "s/<my_domain_app>/${2}/g" docker-compose.yml
+sed -i "s/info@<my_domain_app>/${3}/g" docker-compose.yml
+sed -i "s/<bd_password>/${4}/g" docker-compose.yml
+sed -i "s/<user_bdd>/${5}/g" docker-compose.yml
+sed -i "s/<user_wp>/${6}/g" docker-compose.yml
+sed -i "s/<wp_user_pass>/${7}/g" docker-compose.yml
 
+# Crear una carpeta con el nombre del sitio para almacenar sus volumnes
+mkdir ${1}
+
+# Crear carpeta wp
+mkdir ${1}/wp
+
+# Crear carpeta db
+mkdir ${1}/db
+
+# Crear carpeta log
+mkdir ${1}/log
+
+# Cambiamos el owner de la carpeta y todas su subcarpetas por el usuario 10001o00
+chown -R 1000:1000 ${1}
+
+# Levantamos el docker-compose.yml
+docker-compose up -d
