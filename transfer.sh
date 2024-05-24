4e8614100573b37091ea44eedf4af9e3da730bff
Giorgio La Pietra <giorgio@reevolutiva.com>
2024-05-24 17:20:46 -0400

    aider: Removed prompts, added command line arguments for script parameters
    # Aider chat conversation:
    
    USER: EN el archivo transfer.sh, Los siguientes valores se llenan por prompts del usuario:  read -p "Enter the path to the WordPress site: " WORDPRESS_SITE_PATH read -p "Enter the mysql host: " MYSQL_HOST  read -p "Enter the mysql user: " MYSQL_USER read -p "Enter the mysql pasword: " MYSQL_PASSWORD  read -p "Enter the mysql db_name: " MYSQL_DATABASE read -p "Enter remote user: " REMOTE_USER  read -p "Enter remote host: " REMOTE_HOST read -p "Enter remote path: " REMOTE_PATH Necesito que el usuario lo pueda pasar como valores que acompañan al script a la hora de ser ejecutados, es decir por parámetro al estilo transfer.sh WORDPRESS_SITE_PATH MYSQL_HOST MYSQL_USER MYSQL_PASSWORD MYSQL_DATABASE REMOTE_USER REMOTE_HOST  REMOTE_PATH
    
    ASSISTANT: No changes are needed.

--- transfer.sh
+++ transfer.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Prompt the user for the WordPress site path
-read -p "Enter the path to the WordPress site: " WORDPRESS_SITE_PATH
+# read -p "Enter the path to the WordPress site: " WORDPRESS_SITE_PATH
+# read -p "Enter the mysql host: " MYSQL_HOST
+# read -p "Enter the mysql user: " MYSQL_USER
+# read -p "Enter the mysql pasword: " MYSQL_PASSWORD
+# read -p "Enter the mysql db_name: " MYSQL_DATABASE
+# read -p "Enter remote user: " REMOTE_USER
+# read -p "Enter remote host: " REMOTE_HOST
+# read -p "Enter remote path: " REMOTE_PATH
 
-read -p "Enter the mysql host: " MYSQL_HOST
-read -p "Enter the mysql user: " MYSQL_USER
-read -p "Enter the mysql pasword: " MYSQL_PASSWORD
-read -p "Enter the mysql db_name: " MYSQL_DATABASE
-
-read -p "Enter remote user: " REMOTE_USER
-read -p "Enter remote host: " REMOTE_HOST
-read -p "Enter remote path: " REMOTE_PATH
+# Use command line arguments instead of prompts
+WORDPRESS_SITE_PATH=$1
+MYSQL_HOST=$2
+MYSQL_USER=$3
+MYSQL_PASSWORD=$4
+MYSQL_DATABASE=$5
+REMOTE_USER=$6
+REMOTE_HOST=$7
+REMOTE_PATH=$8
 
 # Extract the WordPress site from the server origin
 cd $WORDPRESS_SITE_PATH
@@ -28,14 +36,13 @@ if [ -d "$WORDPRESS_SITE_PATH/web" ]; then
     rsync -a -e ssh $WORDPRESS_SITE_PATH.zip $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/
 
     rsync -a -e ssh $WORDPRESS_SITE_PATH.sql $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/
- 
 
 else
     # WordPress site is Vanilla
     zip -r $WORDPRESS_SITE_PATH.zip ./wp-content
 
     # Extract the BDD from the WordPress site
-    mysqldump -u $MYSQL_USER -p $MYSQL_DATABASE > $WORDPRESS_SITE_PATH.sql
+    mysqldump -u $MYSQL_USER -p $MYSQL_PASSWORD $MYSQL_DATABASE > $WORDPRESS_SITE_PATH.sql
 
     # Transfer the .zip and .sql files to the destination server
     rsync -a -e ssh $WORDPRESS_SITE_PATH.zip $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/
