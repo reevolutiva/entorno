d8f18981941030e324e26854339e23eb74c9de72
= <=>
2024-05-31 12:18:42 -0400

    Corrected flag from `-src` to `--src` and commented out docker compose down command in diactivate-container.sh

--- diactivate-container.sh
+++ diactivate-container.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Check if the flag is provided
-if [ "$1" == "-src" ]; then
+if [ "$1" == "--src" ]; then
     # Get the path to docker-compose.yml
     DOCKER_COMPOSE_PATH=$(find . -name "docker-compose.yml" -print -quit)
+
+    echo $DOCKER_COMPOSE_PATH
 
     # Check if the file exists
     if [ -f "$DOCKER_COMPOSE_PATH" ]; then
         # Execute docker compose down
-        docker compose -f "$DOCKER_COMPOSE_PATH" down
+        echo "$DOCKER_COMPOSE_PATH" 
+        #docker compose -f "$DOCKER_COMPOSE_PATH" down
     else
         echo "docker-compose.yml not found"
     fi
