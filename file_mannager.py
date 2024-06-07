9faf5e43df41c3208c6ac102b2f00feef950d5f0
= <=>
2024-06-07 09:34:12 -0400

    save

--- file_mannager.py
+++ file_mannager.py
@@ -1,11 +1,19 @@
 import os
+import configparser
 
 def list_directories(path):
     return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
+def read_env_file(path):
+    env_vars = {}
+    if os.path.exists(path) and os.path.isfile(path):
+        config = configparser.ConfigParser()
+        config.read(path)
+        for section in config.sections():
+            for key, value in config.items(section):
+                env_vars[key] = value
+    return env_vars
+
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) != 2:
-        print("Usage: python file_mannager.py <path>")
-    else:
-        print(list_directories(sys.argv[1]))
+    res = list_directories("/home/jerexxypunto/entorno/")
+    print(res)
