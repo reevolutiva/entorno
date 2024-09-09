3f01874e7c056f00e149f9c5d1147b8ddca28cb1
Jere MAC <jerexxypunto@gmail.com>
2024-09-09 11:31:28 -0300

    adapted to recive site

--- env_install/env_install.sh
+++ env_install/env_install.sh
@@ -1,7 +1,7 @@
 echo "Instalando entorno reev-host"
-conda env create -n reev-host -y -f environment.yml
+$HOME/miniconda/bin/conda env create -n reev-host -y -f environment.yml
 
 # Activamos el entorno
 source activate reev-host
-conda activate reev-host
+$HOME/miniconda/bin/conda activate reev-host
 echo "Entorno reev-host instalado"
--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 # Instalamos las dependencias del proyecto
 pip install -r requeriments.txt
 
-$HOME/miniconda/envs/reev-host/bin/uvicorn app:app --reload
+$HOME/miniconda/envs/reev-host/bin/uvicorn app:app --host 138.219.43.82 --port 2525 --reload
